2dcaa6f15dde539cf5154b44899c1b7fa5adba3a
RogelioSanchezD <98624560+RogelioSanchezD@users.noreply.github.com>
2023-08-23 05:07:53 -0600

    Fix Avance Anterior
    Creación Menú Principal

--- Biblioteca Casa de las palabras/src/main/java/frMenu/Menú_Principal.java
+++ Biblioteca Casa de las palabras/src/main/java/frMenu/Menú_Principal.java
@@ -15,6 +15,9 @@ public class Menú_Principal extends javax.swing.JFrame {
      */
     public Menú_Principal() {
         initComponents();
+        setTitle("Menú Principal");
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
 
     /**
@@ -26,21 +29,196 @@ public class Menú_Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jButton5 = new javax.swing.JButton();
+        BOTONCATALOGO = new javax.swing.JButton();
+        BOTONSOCIOS = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        BOTONEMPLEADOS = new javax.swing.JButton();
+        BOTON = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 204));
+
+        jButton5.setBackground(new java.awt.Color(255, 0, 0));
+        jButton5.setText("X");
+        jButton5.setToolTipText("");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        BOTONCATALOGO.setBackground(new java.awt.Color(0, 204, 204));
+        BOTONCATALOGO.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        BOTONCATALOGO.setForeground(new java.awt.Color(0, 0, 0));
+        BOTONCATALOGO.setIcon(new javax.swing.ImageIcon(getClass().getResource("/static/Recursos/buscar.png"))); // NOI18N
+        BOTONCATALOGO.setText("CATALOGO");
+        BOTONCATALOGO.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BOTONCATALOGOActionPerformed(evt);
+            }
+        });
+
+        BOTONSOCIOS.setBackground(new java.awt.Color(0, 204, 204));
+        BOTONSOCIOS.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        BOTONSOCIOS.setForeground(new java.awt.Color(0, 0, 0));
+        BOTONSOCIOS.setIcon(new javax.swing.ImageIcon(getClass().getResource("/static/Recursos/socios (1).png"))); // NOI18N
+        BOTONSOCIOS.setText("SOCIOS");
+        BOTONSOCIOS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BOTONSOCIOSActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("BIBLIOTECA");
+
+        BOTONEMPLEADOS.setBackground(new java.awt.Color(0, 204, 204));
+        BOTONEMPLEADOS.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        BOTONEMPLEADOS.setForeground(new java.awt.Color(0, 0, 0));
+        BOTONEMPLEADOS.setIcon(new javax.swing.ImageIcon(getClass().getResource("/static/Recursos/empleados.png"))); // NOI18N
+        BOTONEMPLEADOS.setText("EMPLEADOS");
+        BOTONEMPLEADOS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BOTONEMPLEADOSActionPerformed(evt);
+            }
+        });
+
+        BOTON.setBackground(new java.awt.Color(0, 204, 204));
+        BOTON.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        BOTON.setForeground(new java.awt.Color(0, 0, 0));
+        BOTON.setText("???");
+        BOTON.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BOTONActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("sansserif", 1, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("LA CASA DE LAS PALABRAS");
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/static/Recursos/biblioteca.png"))); // NOI18N
+        jLabel3.setText("jLabel3");
+
+        jLabel4.setFont(new java.awt.Font("sansserif", 1, 36)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("BIENVENIDO");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel1)
+                        .addGap(240, 240, 240))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(BOTONCATALOGO, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(BOTONSOCIOS, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(BOTONEMPLEADOS)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(BOTON, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(12, 12, 12))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(154, 154, 154))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(209, 209, 209)
+                .addComponent(jLabel4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BOTONCATALOGO, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BOTONSOCIOS, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BOTONEMPLEADOS, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BOTON, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void BOTONCATALOGOActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BOTONCATALOGOActionPerformed
+        // TODO add your handling code here:
+        //Catalogo catalogoForm = new Catalogo();
+
+        // Mostrar el formulario "Catalogo"
+        //catalogoForm.setVisible(true);
+
+    }//GEN-LAST:event_BOTONCATALOGOActionPerformed
+
+    private void BOTONSOCIOSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BOTONSOCIOSActionPerformed
+        // TODO add your handling code here:
+        frSocios frSociosForm = new frSocios();
+
+        // Mostrar el formulario "Socios"
+        frSociosForm.setVisible(true);
+
+    }//GEN-LAST:event_BOTONSOCIOSActionPerformed
+
+    private void BOTONEMPLEADOSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BOTONEMPLEADOSActionPerformed
+        // TODO add your handling code here:
+        Empleados empleadosForm = new Empleados();
+
+        // Mostrar el formulario "Empleados"
+        empleadosForm.setVisible(true);
+    }//GEN-LAST:event_BOTONEMPLEADOSActionPerformed
+
+    private void BOTONActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BOTONActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BOTONActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +256,15 @@ public class Menú_Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BOTON;
+    private javax.swing.JButton BOTONCATALOGO;
+    private javax.swing.JButton BOTONEMPLEADOS;
+    private javax.swing.JButton BOTONSOCIOS;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
